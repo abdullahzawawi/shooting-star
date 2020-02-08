4bebd78d912e8ca38166ed09c41cf35960c217ef
abdullahzawawi <60826862+abdullahzawawi@users.noreply.github.com>
2020-02-08 23:22:27 +0300

    ;-0 files

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D body;
+    public Camera cam;
+    public Vector2 mousePos;
+
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 20.0f;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        Vector2 lookDir = mousePos - body.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg ;
+        body.rotation = angle;
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public Transform firepoint;
+    public GameObject Bulletprefab;
+    public float bulletforce = 20f;
+    
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+        
+    }
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(Bulletprefab, firepoint.position, firepoint.rotation);
+        Rigidbody2D body = bullet.GetComponent<Rigidbody2D>();
+        body.AddForce(firepoint.up * bulletforce, ForceMode2D.Impulse);
+    }
+}
